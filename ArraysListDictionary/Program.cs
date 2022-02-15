f13c3db130d87088992345244d900fedad444443
Victor Okpanachi <okpanachivictor0@gmail.com>
2022-02-15 08:41:39 +0100

    student rooster list

--- ArraysListDictionary/Program.cs
+++ ArraysListDictionary/Program.cs
@@ -27,6 +27,7 @@ namespace ArraysListDictionary
             */
 
 
+            /*
             //create an array of 3 names. ask the user which number to select. when the user gives you a number,, return that name
 
             string[] names = new string[] { "Vicor", "Angela", "Stella" };
@@ -35,6 +36,43 @@ namespace ArraysListDictionary
             int number = Convert.ToInt32(numberInput);
             Console.WriteLine();
             Console.WriteLine(names[number]);
+            */
+
+            /*
+
+            //LISTS
+            List<string> names = new List<string>();
+
+            names.Add("Tim");
+            names.Add("Sue");
+            Console.WriteLine($"Your name is {names[0]} {names[1]}");
+            */
+
+
+            /*
+            //add students to a class rooster until there are no more students. Then print out the count of the students
+
+            //plan
+            //create a list for the rooster
+
+            List<string> rooster = new List<string>();
+
+            //add students to the list
+            string reply;
+            do
+            {
+                Console.Write("Enter names of students: ");
+                string name = Console.ReadLine();
+                rooster.Add(name);
+                Console.Write("Do you want to add another name to a rooster? Yes or no: ");
+                reply = Console.ReadLine();
+                
+
+            } while (reply.ToLower() == "yes");
+
+            Console.WriteLine($" The total number of students = {rooster.Count}");
+            */
+
 
 
             Console.ReadLine();
