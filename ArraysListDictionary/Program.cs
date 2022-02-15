a9657697e3e6bb879a9f57b8734f078b67359922
Victor Okpanachi <okpanachivictor0@gmail.com>
2022-02-15 08:12:57 +0100

    Array3

--- ArraysListDictionary/Program.cs
+++ ArraysListDictionary/Program.cs
@@ -14,11 +14,27 @@ namespace ArraysListDictionary
              firstNames[0] = "Victor";
              firstNames[1] = "Oluwaseun";
              Console.WriteLine($"Welcome {firstNames[0]} and {firstNames[1]}");
-            */
+            
 
             string data = "Victor, Fibechola, Okpanachi";
             string[] fullNames = data.Split(',');
             Console.WriteLine($"Your fullname is {fullNames[2]}, {fullNames[1]} {fullNames[0]}");
+           
+
+            
+            string[] names = new string[] { "Sue", "Jone", "Angela" };
+            Console.WriteLine(names[2]);
+            */
+
+
+            //create an array of 3 names. ask the user which number to select. when the user gives you a number,, return that name
+
+            string[] names = new string[] { "Vicor", "Angela", "Stella" };
+            Console.Write("From 0 to 2 which number do you want to select?\n");
+            string numberInput = Console.ReadLine();
+            int number = Convert.ToInt32(numberInput);
+            Console.WriteLine();
+            Console.WriteLine(names[number]);
 
 
             Console.ReadLine();
