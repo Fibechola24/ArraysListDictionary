56b32da3f1b5a72d4737097bde126f031ec29750
Victor Okpanachi <okpanachivictor0@gmail.com>
2022-02-15 07:59:52 +0100

    array2

--- ArraysListDictionary/Program.cs
+++ ArraysListDictionary/Program.cs
@@ -10,10 +10,15 @@ namespace ArraysListDictionary
     {
         static void Main(string[] args)
         {
-            string [] firstNames = new string[2];
-            firstNames[0] = "Victor";
-            firstNames[1] = "Oluwaseun";
-            Console.WriteLine($"Welcome {firstNames[0]} and {firstNames[1]}");
+            /* string [] firstNames = new string[2];
+             firstNames[0] = "Victor";
+             firstNames[1] = "Oluwaseun";
+             Console.WriteLine($"Welcome {firstNames[0]} and {firstNames[1]}");
+            */
+
+            string data = "Victor, Fibechola, Okpanachi";
+            string[] fullNames = data.Split(',');
+            Console.WriteLine($"Your fullname is {fullNames[2]}, {fullNames[1]} {fullNames[0]}");
 
 
             Console.ReadLine();
